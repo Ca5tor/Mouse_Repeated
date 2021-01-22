2989e7356f1146203137bbdf6232e8a61039a496
Ca5tor <48414173+Ca5tor@users.noreply.github.com>
2021-01-22 08:29:00 +0300

    +интерфейс(без реализации) +сразу скрывается в трее

--- MouseRepeated/MyForm.h
+++ MouseRepeated/MyForm.h
@@ -38,6 +38,14 @@ namespace MouseRepeated {
 	private: System::Windows::Forms::ContextMenuStrip^ contextMenuStrip1;
 	private: System::Windows::Forms::ToolStripMenuItem^ настройкиToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ выходToolStripMenuItem;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	private: System::Windows::Forms::CheckBox^ checkBox3;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Button^ button1;
 
 	protected:
 
@@ -69,6 +77,14 @@ namespace MouseRepeated {
 			this->выходToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->bt_exit = (gcnew System::Windows::Forms::Button());
 			this->bt_roll_up = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->contextMenuStrip1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -87,14 +103,14 @@ namespace MouseRepeated {
 					this->выходToolStripMenuItem
 			});
 			this->contextMenuStrip1->Name = L"contextMenuStrip1";
-			this->contextMenuStrip1->Size = System::Drawing::Size(181, 70);
+			this->contextMenuStrip1->Size = System::Drawing::Size(135, 48);
 			// 
 			// настройкиToolStripMenuItem
 			// 
 			this->настройкиToolStripMenuItem->BackColor = System::Drawing::Color::DimGray;
 			this->настройкиToolStripMenuItem->ForeColor = System::Drawing::Color::White;
 			this->настройкиToolStripMenuItem->Name = L"настройкиToolStripMenuItem";
-			this->настройкиToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->настройкиToolStripMenuItem->Size = System::Drawing::Size(134, 22);
 			this->настройкиToolStripMenuItem->Text = L"Настройки";
 			this->настройкиToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::настройкиToolStripMenuItem_Click);
 			// 
@@ -103,35 +119,137 @@ namespace MouseRepeated {
 			this->выходToolStripMenuItem->BackColor = System::Drawing::Color::DimGray;
 			this->выходToolStripMenuItem->ForeColor = System::Drawing::Color::White;
 			this->выходToolStripMenuItem->Name = L"выходToolStripMenuItem";
-			this->выходToolStripMenuItem->Size = System::Drawing::Size(180, 22);
+			this->выходToolStripMenuItem->Size = System::Drawing::Size(134, 22);
 			this->выходToolStripMenuItem->Text = L"Выход";
 			this->выходToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::выходToolStripMenuItem_Click);
 			// 
 			// bt_exit
 			// 
-			this->bt_exit->Location = System::Drawing::Point(13, 338);
+			this->bt_exit->BackColor = System::Drawing::Color::DarkSeaGreen;
+			this->bt_exit->ForeColor = System::Drawing::Color::White;
+			this->bt_exit->Location = System::Drawing::Point(13, 281);
 			this->bt_exit->Name = L"bt_exit";
-			this->bt_exit->Size = System::Drawing::Size(75, 49);
+			this->bt_exit->Size = System::Drawing::Size(84, 49);
 			this->bt_exit->TabIndex = 0;
 			this->bt_exit->Text = L"Завершить работу";
-			this->bt_exit->UseVisualStyleBackColor = true;
+			this->bt_exit->UseVisualStyleBackColor = false;
 			this->bt_exit->Click += gcnew System::EventHandler(this, &MyForm::bt_exit_Click);
 			// 
 			// bt_roll_up
 			// 
-			this->bt_roll_up->Location = System::Drawing::Point(94, 338);
+			this->bt_roll_up->BackColor = System::Drawing::Color::DarkSeaGreen;
+			this->bt_roll_up->ForeColor = System::Drawing::Color::White;
+			this->bt_roll_up->Location = System::Drawing::Point(99, 281);
 			this->bt_roll_up->Name = L"bt_roll_up";
-			this->bt_roll_up->Size = System::Drawing::Size(72, 49);
+			this->bt_roll_up->Size = System::Drawing::Size(82, 49);
 			this->bt_roll_up->TabIndex = 1;
 			this->bt_roll_up->Text = L"Свернуть в трей";
-			this->bt_roll_up->UseVisualStyleBackColor = true;
+			this->bt_roll_up->UseVisualStyleBackColor = false;
 			this->bt_roll_up->Click += gcnew System::EventHandler(this, &MyForm::bt_roll_up_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->ForeColor = System::Drawing::Color::White;
+			this->checkBox1->Location = System::Drawing::Point(13, 71);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(86, 17);
+			this->checkBox1->TabIndex = 2;
+			this->checkBox1->Text = L"Копировать";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->ForeColor = System::Drawing::Color::White;
+			this->checkBox2->Location = System::Drawing::Point(13, 94);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(73, 17);
+			this->checkBox2->TabIndex = 3;
+			this->checkBox2->Text = L"Вставить";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->ForeColor = System::Drawing::Color::White;
+			this->checkBox3->Location = System::Drawing::Point(13, 117);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(76, 17);
+			this->checkBox3->TabIndex = 4;
+			this->checkBox3->Text = L"Вырезать";
+			this->checkBox3->UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Только текст", L"Все типы файлов" });
+			this->comboBox1->Location = System::Drawing::Point(12, 172);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(169, 21);
+			this->comboBox1->TabIndex = 5;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::White;
+			this->label1->Location = System::Drawing::Point(10, 45);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(96, 17);
+			this->label1->TabIndex = 6;
+			this->label1->Text = L"Отображение:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->ForeColor = System::Drawing::Color::White;
+			this->label2->Location = System::Drawing::Point(12, 152);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(141, 17);
+			this->label2->TabIndex = 7;
+			this->label2->Text = L"Высвечивать кнопки:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->ForeColor = System::Drawing::Color::White;
+			this->label3->Location = System::Drawing::Point(53, 9);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(75, 17);
+			this->label3->TabIndex = 8;
+			this->label3->Text = L"Настройки";
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::CornflowerBlue;
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Location = System::Drawing::Point(12, 213);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(169, 49);
+			this->button1->TabIndex = 9;
+			this->button1->Text = L"Сохранить настройки";
+			this->button1->UseVisualStyleBackColor = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
-			this->ClientSize = System::Drawing::Size(306, 399);
+			this->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->ClientSize = System::Drawing::Size(194, 348);
 			this->ControlBox = false;
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->checkBox3);
+			this->Controls->Add(this->checkBox2);
+			this->Controls->Add(this->checkBox1);
 			this->Controls->Add(this->bt_roll_up);
 			this->Controls->Add(this->bt_exit);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
@@ -140,9 +258,11 @@ namespace MouseRepeated {
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Mouse Repeated";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->Resize += gcnew System::EventHandler(this, &MyForm::MyForm_Resize);
 			this->contextMenuStrip1->ResumeLayout(false);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
@@ -153,20 +273,24 @@ namespace MouseRepeated {
 			WindowState = FormWindowState::Normal;	// Разворачиваем окно
 		}
 
+		void hide_form() {							// Скрыть форму и отправить уведомление
+			if (this->WindowState == FormWindowState::Minimized) {
+				this->ShowInTaskbar = false;		// Прячем наше окно из панели
+				notifyIcon1->Visible = true;		// Делаем нашу иконку в трее активной
+			}
+
+			// Уведомление что программа свернулась в трей
+			notifyIcon1->BalloonTipTitle = "Mouse Repeated";
+			notifyIcon1->BalloonTipText = "Я спрятана в трей и продолжу свою работу.";
+			notifyIcon1->ShowBalloonTip(1000);
+		}
+
 	private: System::Void notifyIcon1_MouseDoubleClick(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
 		open_form();
 	}
 
 	private: System::Void MyForm_Resize(System::Object^ sender, System::EventArgs^ e) {
-		if (this->WindowState == FormWindowState::Minimized) {
-			this->ShowInTaskbar = false;		// Прячем наше окно из панели
-			notifyIcon1->Visible = true;		// Делаем нашу иконку в трее активной
-		}
-
-		// Уведомление что программа свернулась в трей
-		notifyIcon1->BalloonTipTitle = "Mouse Repeated";
-		notifyIcon1->BalloonTipText = "Я спрятана в трей и продолжу свою работу.";
-		notifyIcon1->ShowBalloonTip(1000);
+		hide_form();		
 	}
 
 /////////////////		Кнопки		/////////////////
@@ -187,6 +311,11 @@ namespace MouseRepeated {
 		open_form();
 	}
 ///////////////////////////////////////////////////////////////
+
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	this->WindowState = FormWindowState::Minimized; // Сварачиваем форму
+	hide_form();
+}
 
 };
 }
